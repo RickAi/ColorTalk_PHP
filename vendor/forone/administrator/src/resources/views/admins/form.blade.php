c00e9a5bd529c99d36389d63cf34d1f4e29f33fd
YogiAi <ayb854@163.com>
2016-04-17 21:04:28 +0800

    final edit

--- vendor/forone/administrator/src/resources/views/admins/form.blade.php
+++ vendor/forone/administrator/src/resources/views/admins/form.blade.php
@@ -3,7 +3,7 @@
 {!! Form::group_text('email','Email','please enter email') !!}
 
 @if (str_is('admin.admins.create', Route::current()->getName()))
-    {!! Form::group_password('password','password','please enter password') !!}
+    {!! Form::group_password('password', 'Password','please enter password') !!}
 @endif
 
 @section('js')
